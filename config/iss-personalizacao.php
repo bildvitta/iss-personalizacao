1e6b975c0b721b0abc2b90dcbf743123bc174191
Michael <cocaoaoao@gmail.com>
2022-01-19 10:36:50 -0300

    Update README.md

--- config/iss-personalizacao.php
+++ config/iss-personalizacao.php
@@ -2,5 +2,5 @@
 
 return [
     'base_uri' => env('MS_PERSONALIZACAO_BASE_URI', 'https://api-dev-personalizacao.nave.dev'),
-    'prefix' => env('MS_PERSONALIZACAO_API_PREFIX', '/api')
+    'prefix' => env('MS_PERSONALIZACAO_API_PREFIX', '/api'),
 ];
